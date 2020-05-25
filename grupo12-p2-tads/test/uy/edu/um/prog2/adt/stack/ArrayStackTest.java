2fceb98136e81198c00c5785b7cd07c1d1cb8c55
Renzo D'Ambrosio <renzo.dam.ze@gmail.com>
2020-05-25 15:51:54 -0300

    Organize test packages

---
+++ grupo12-p2-tads/test/uy/edu/um/prog2/adt/stack/ArrayStackTest.java
@@ -0,0 +1,39 @@
+package uy.edu.um.prog2.adt.stack;
+
+import org.junit.jupiter.api.Test;
+import uy.edu.um.prog2.adt.exeptions.EmptyStack;
+import uy.edu.um.prog2.adt.implementations.ArrayStack;
+import uy.edu.um.prog2.adt.interfaces.MyStack;
+
+import static org.junit.Assert.assertEquals;
+
+class ArrayStackTest {
+
+    @Test
+    void push() throws EmptyStack {
+        MyStack<Integer> staArray=new ArrayStack<Integer>(6);
+        staArray.push(3);
+        staArray.push(1);
+        staArray.push(6);
+
+        assertEquals((Integer)6, staArray.top());
+    }
+
+    @Test
+    void pop() throws EmptyStack {
+        MyStack<Integer> staArray=new ArrayStack<>(6);
+        staArray.push(3);
+        staArray.push(1);
+        staArray.push(6);
+        assertEquals((Integer)6, staArray.pop());
+    }
+
+    @Test
+    void top() throws EmptyStack {
+        MyStack<Integer> staArray=new ArrayStack<>(6);
+        staArray.push(3);
+        staArray.push(1);
+        staArray.push(6);
+        assertEquals((Integer)6, staArray.top());
+    }
+}
